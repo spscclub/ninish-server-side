4dfaf5adb595f1602f61d17e170e053190d5f568
spscclub <155471423+spscclub@users.noreply.github.com>
2024-01-03 16:47:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 
-const uri = `mongodb+srv://NthoAdmin:QgdP0tEfpOwyvLMh@cluster0.ydunwkf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydunwkf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
